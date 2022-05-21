d8c9dd71c583ce670bf1010f116bc014e8d0dda2
Jude-Cromarty <judecromarty@aol.com>
2022-05-21 23:16:00 +0100

    added end screen
    added a prompt to press space to restart when someone wins

--- HeartBeatEmUp/Assets/Scripts/GameOvers.cs
+++ HeartBeatEmUp/Assets/Scripts/GameOvers.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameOvers : MonoBehaviour
 {
+    public KeyCode EndCode;
     public timer tomer;
     public Player1 player1;
     public Player2 player2;
@@ -22,15 +24,33 @@ public class GameOvers : MonoBehaviour
         if(tomer.cntdnw <= 0)
         {
             Winner = "Draw!";
+            GameOver.text = (Winner);
+            StartCoroutine(WaitingForTheNight());
+            if(Input.GetKey(EndCode))
+                {SceneManager.LoadScene("SampleScene");}
         }
         else if(player1.currentHealth <= 0)
         {
             Winner ="Player 2 Wins!";
+            GameOver.text = (Winner);
+            StartCoroutine(WaitingForTheNight());
+            if(Input.GetKey(EndCode))
+                {SceneManager.LoadScene("SampleScene");}
         }
         else if(player2.currentHealth <= 0)
         {
-            Winner ="Player 1 Wins!";
+            Winner = ("Player 1 Wins!");
+            GameOver.text = (Winner);
+            StartCoroutine(WaitingForTheNight());
+            if(Input.GetKey(EndCode))
+                {SceneManager.LoadScene("SampleScene");}
         }
+    }
+
+    IEnumerator WaitingForTheNight()
+    {
+        yield return new WaitForSeconds(5);
+        Winner = ("Press space to restart");
         GameOver.text = (Winner);
     }
 }
